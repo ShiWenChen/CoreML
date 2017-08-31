976d657cddb156d09f30f5618950472311af0382
Chenshiwen <cswzmg@live.com>
2017-08-31 16:47:35 +0800

    Initial Commit

---
+++ CoreML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreML
+//
+//  Created by ShiWen on 2017/8/31.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
